85c77f24858a0c9ea4a1fc0144cd10eeaba6325d
blessingyula0-source <blessingyula0@gmail.com>
2026-06-04 11:55:49 +0300

    lab 2.7JS101

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let age = 20;
+let name = "Blessing";
+var course = "Computer Science";
+const university = "Strathmore University";
+
+//Redeclare.
+var course = "Computer Science";
+course = "Information Technology";
+
+if (true) {
+    let time = "11:30";
+    var day ='4th';
+    const year = 2026; 
+}
+console.log(time);
