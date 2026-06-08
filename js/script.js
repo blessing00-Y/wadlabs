9f1b13c1c42d59f42e169e4095486965975eb41a
blessingyula0-source <blessingyula0@gmail.com>
2026-06-08 13:15:48 +0300

    lab 2.8 - js functions

--- js/script.js
+++ js/script.js
@@ -13,3 +13,40 @@ if (true) {
     const year = 2026; 
 }
 console.log(time);
+/**
+ * A function that calculates the area of a rectangle
+ * @param {int} height 
+ * @param {int} width 
+ * @return {int} area of the rectangle
+ */
+//function declaration
+function calculateArea(height, width){
+    if (height==null){
+      console.log("height is missing!")  
+    }
+    else if (width==null){
+        console.log("width is missing!")
+    }else{
+    let area=height*width;
+    return area
+    }
+    //return value
+    //console.log("function calculateArea")
+    //alert("function called/executed")
+}
+//return a value is NOT printing out
+//execution-called
+calculateArea(20,5)//function call
+console.log(calculateArea(20,5))//printing the return value of the function call
+console.log(calculateArea(20))//printing the return value of the function call
+console.log(calculateArea())//printing the return value of the function call
+
+//function expression
+const add=function(num1,num2){
+    return num1+num2;
+}
+console.log(add(5,10))
+
+//arrow function    
+const multiply=(x,y)=>x*y;
+console.log(multiply(5,10));
